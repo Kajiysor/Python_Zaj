33586088f3e0e1af0fb70c3c944735517f8ce123
Kajiysor <mrbarman55@gmail.com>
2022-12-03 13:58:54 +0100

    refactor

--- Connect4_Project/connect4.py
+++ Connect4_Project/connect4.py
@@ -7,9 +7,10 @@ from typing import List, Tuple
 
 
 class Connect4():
+    '''Connect 4 Game Class'''
 
-    # Initialize the game
     def __init__(self, ai_depth: int, row_count=6, column_count=7) -> None:
+        ''' Initialize the gam '''
 
         # Variables for the game
         self.row_count = row_count
@@ -23,6 +24,7 @@ class Connect4():
         self.__player_piece = 1
         self.__ai_piece = 2
         self.four_pieces = 4
+        self.MAX = 100000000000000
 
         # Variables for the board
         self.board = np.zeros((self.row_count, self.column_count))
@@ -38,46 +40,86 @@ class Connect4():
         self.__red = (255, 0, 0)
         self.__yellow = (255, 255, 0)
 
-    # Prints the board to the console
     def __print_board(self, board: np.ndarray) -> None:
+        ''' Prints the board to the console '''
         print(np.flip(board, 0))
 
-    # Draws the board to the screen
     def __draw_board(self, board: np.ndarray, screen) -> None:
+        ''' Draws the board to the screen '''
         for c in range(self.column_count):
             for r in range(self.row_count):
-                pygame.draw.rect(screen, self.__blue, (c * self.__square_size, r *
-                                 self.__square_size + self.__square_size, self.__square_size, self.__square_size))
-                pygame.draw.circle(screen, self.__black, (int(c * self.__square_size + self.__square_size / 2), int(
-                    r * self.__square_size + self.__square_size + self.__square_size / 2)), self.__radius)
+                pygame.draw.rect(
+                    screen,
+                    self.__blue,
+                    (c *
+                     self.__square_size,
+                     r *
+                     self.__square_size +
+                     self.__square_size,
+                     self.__square_size,
+                     self.__square_size))
+                pygame.draw.circle(
+                    screen,
+                    self.__black,
+                    (c *
+                     self.__square_size +
+                     self.__square_size //
+                     2,
+                     r *
+                     self.__square_size +
+                     self.__square_size +
+                     self.__square_size //
+                     2),
+                    self.__radius)
 
         for c in range(self.column_count):
             for r in range(self.row_count):
                 if board[r][c] == self.__player_piece:
-                    pygame.draw.circle(screen, self.__red, (int(c * self.__square_size + self.__square_size / 2),
-                                       self.__height - int(r * self.__square_size + self.__square_size / 2)), self.__radius)
+                    pygame.draw.circle(
+                        screen, self.__red,
+                        ((c *
+                          self.__square_size +
+                          self.__square_size //
+                          2), self.__height -
+                         (r *
+                          self.__square_size +
+                          self.__square_size //
+                          2)), self.__radius)
+
                 elif board[r][c] == self.__ai_piece:
-                    pygame.draw.circle(screen, self.__yellow, (int(c * self.__square_size + self.__square_size / 2),
-                                       self.__height - int(r * self.__square_size + self.__square_size / 2)), self.__radius)
+                    pygame.draw.circle(
+                        screen, self.__yellow,
+                        ((c *
+                          self.__square_size +
+                          self.__square_size //
+                          2), self.__height -
+                         (r *
+                          self.__square_size +
+                          self.__square_size //
+                          2)), self.__radius)
 
         pygame.display.update()
 
-    # Drops a piece in the specified row and column
-    def __drop_piece(self, board: np.ndarray, row: int, col: int, piece: int) -> None:
+    def __drop_piece(self, board: np.ndarray, row: int,
+                     col: int, piece: int) -> None:
+        ''' Drops a piece in the specified row and column '''
         board[row][col] = piece
 
-    # Returns True if the specified column is valid
     def __is_valid_location(self, board: np.ndarray, col: int) -> bool:
+        ''' Returns True if the specified column is valid '''
         return board[self.row_count - 1][col] == self.empty
 
-    # Returns the row of the next available space in the specified column
     def __get_next_open_row(self, board: np.ndarray, col: int) -> int:
+        '''
+        Returns the row of the next available
+        space in the specified colum
+        '''
         for row in range(self.row_count):
             if board[row][col] == self.empty:
                 return row
 
-    # Returns a list of valid columns
     def __get_valid_locations(self, board: np.ndarray) -> List[int]:
+        ''' Returns a list of valid columns '''
         valid_locations = []
 
         for col in range(self.column_count):
@@ -86,10 +128,13 @@ class Connect4():
 
         return valid_locations
 
-    # Returns the score of pieces in a row
     def __eval_score_in_a_row(self, window: List[int], piece: int) -> int:
+        ''' Returns the score of pieces in a row '''
         score = 0
-        opponent_piece = self.__ai_piece if piece == self.__player_piece else self.__player_piece
+        if piece == self.__player_piece:
+            opponent_piece = self.__ai_piece
+        else:
+            opponent_piece = self.__player_piece
 
         if window.count(piece) == 4:
             score += 10000
@@ -98,30 +143,31 @@ class Connect4():
         elif window.count(piece) == 2 and window.count(self.empty) == 2:
             score += 5
 
-        if window.count(opponent_piece) == 3 and window.count(self.empty) == 1:
+        if (window.count(opponent_piece) == 3
+                and window.count(self.empty) == 1):
             score -= 13
 
         return score
 
-    # Returns the score of the board
     def __eval_board_score(self, board: np.ndarray, piece: int) -> int:
+        ''' Returns the score of the board '''
         score = 0
 
         # Score center column
-        center_array = [int(i) for i in list(board[:, self.column_count // 2])]
+        center_array = [i for i in list(board[:, self.column_count // 2])]
         center_count = center_array.count(piece)
         score += center_count * 3
 
         # Score horizontal
         for r in range(self.row_count):
-            row_array = [int(i) for i in list(board[r, :])]
+            row_array = [i for i in list(board[r, :])]
             for c in range(self.column_count - 3):
                 window = row_array[c:c + self.four_pieces]
                 score += self.__eval_score_in_a_row(window, piece)
 
         # Score vertical
         for c in range(self.column_count):
-            col_array = [int(i) for i in list(board[:, c])]
+            col_array = [i for i in list(board[:, c])]
             for r in range(self.row_count - 3):
                 window = col_array[r:r + self.four_pieces]
                 score += self.__eval_score_in_a_row(window, piece)
@@ -129,7 +175,8 @@ class Connect4():
         # Score positive sloped diagonal
         for r in range(self.row_count - 3):
             for c in range(self.column_count - 3):
-                window = [board[r + i][c + i] for i in range(self.four_pieces)]
+                window = ([board[r + i][c + i]
+                          for i in range(self.four_pieces)])
                 score += self.__eval_score_in_a_row(window, piece)
 
         # Score negative sloped diagonal
@@ -141,45 +188,64 @@ class Connect4():
 
         return score
 
-    # Returns True if the move is a winning move
     def __is_winning_move(self, board: np.ndarray, piece: int) -> bool:
+        ''' Returns True if the move is a winning move '''
 
         # Check horizontal locations for win
         for c in range(self.column_count - 3):
             for r in range(self.row_count):
-                if board[r][c] == piece and board[r][c + 1] == piece and board[r][c + 2] == piece and board[r][c + 3] == piece:
+                if ((board[r][c] == piece)
+                        and (board[r][c + 1] == piece)
+                        and (board[r][c + 2] == piece)
+                        and (board[r][c + 3] == piece)):
                     return True
 
         # Check vertical locations for win
         for c in range(self.column_count):
             for r in range(self.row_count - 3):
-                if board[r][c] == piece and board[r + 1][c] == piece and board[r + 2][c] == piece and board[r + 3][c] == piece:
+                if ((board[r][c] == piece)
+                        and (board[r + 1][c] == piece)
+                        and (board[r + 2][c] == piece)
+                        and (board[r + 3][c] == piece)):
                     return True
 
         # Check positively sloped diaganols
         for c in range(self.column_count - 3):
             for r in range(self.row_count - 3):
-                if board[r][c] == piece and board[r + 1][c + 1] == piece and board[r + 2][c + 2] == piece and board[r + 3][c + 3] == piece:
+                if ((board[r][c] == piece)
+                        and (board[r + 1][c + 1] == piece)
+                        and (board[r + 2][c + 2] == piece)
+                        and (board[r + 3][c + 3] == piece)):
                     return True
 
         # Check negatively sloped diaganols
         for c in range(self.column_count - 3):
             for r in range(3, self.row_count):
-                if board[r][c] == piece and board[r - 1][c + 1] == piece and board[r - 2][c + 2] == piece and board[r - 3][c + 3] == piece:
+                if ((board[r][c] == piece)
+                        and (board[r - 1][c + 1] == piece)
+                        and (board[r - 2][c + 2] == piece)
+                        and (board[r - 3][c + 3] == piece)):
                     return True
 
-    # Returns the best move for the AI
-    def __minimax(self, board: np.ndarray, depth: int, alpha=-math.inf, beta=math.inf, maximizing_player=True) -> Tuple[int, int]:
+    def __minimax(self, board: np.ndarray, depth: int, alpha=-math.inf,
+                  beta=math.inf, maximizing_player=True) -> Tuple[int, int]:
+        ''' Returns the best move for the AI '''
+
         valid_locations = self.__get_valid_locations(board)
-        is_terminal = self.__is_winning_move(board, self.__player_piece) or self.__is_winning_move(
-            board, self.__ai_piece) or len(self.__get_valid_locations(board)) == 0
+        is_terminal = (
+            self.__is_winning_move(
+                board,
+                self.__player_piece) or self.__is_winning_move(
+                board,
+                self.__ai_piece) or len(
+                self.__get_valid_locations(board)) == 0)
 
         if depth == 0 or is_terminal:
             if is_terminal:
                 if self.__is_winning_move(board, self.__ai_piece):
-                    return (None, 100000000000000)
+                    return (None, self.MAX)
                 elif self.__is_winning_move(board, self.__player_piece):
-                    return (None, -10000000000000)
+                    return (None, -self.MAX)
                 else:
                     return (None, 0)
             else:
@@ -249,8 +315,8 @@ class Connect4():
                                      (0, 0, self.__width, self.__square_size))
                     posx = event.pos[0]
                     if self.__turn == 0:
-                        pygame.draw.circle(screen, self.__red, (posx, int(
-                            self.__square_size / 2)), self.__radius)
+                        pygame.draw.circle(screen, self.__red, (posx, (
+                            self.__square_size // 2)), self.__radius)
 
                 pygame.display.update()
 
@@ -261,14 +327,15 @@ class Connect4():
                     # Ask for Player 1 Input
                     if self.__turn == 0:
                         posx = event.pos[0]
-                        col = int(math.floor(posx / self.__square_size))
+                        col = math.floor(posx / self.__square_size)
 
                         if self.__is_valid_location(self.board, col):
                             row = self.__get_next_open_row(self.board, col)
                             self.__drop_piece(self.board, row,
                                               col, self.__player_piece)
 
-                            if self.__is_winning_move(self.board, self.__player_piece):
+                            if self.__is_winning_move(
+                                    self.board, self.__player_piece):
                                 label = myfont.render(
                                     "Player 1 wins!", 1, self.__red)
                                 screen.blit(label, (40, 10))
